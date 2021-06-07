1d25d39b51b25a52007ca2fba21435ce01323db5
mum567@naver.com <mum567@naver.com>
2021-06-07 09:24:51 +0900

    [1조]

---
+++ PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //토끼 이동 속도
+    float rbMvSpeed = 10.0f;
+
+    //토끼 회전 속도
+    float rbRotSpeed = 150.0f;
+
+    void Start()
+    {
+        
+    }
+
+      void Update()
+    {
+        float mv = Input.GetAxis("Vertical1") * rbMvSpeed * Time.deltaTime;
+        float rot = Input.GetAxis("Horizontal1") * rbRotSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, mv);
+        transform.Rotate(0, rot, 0);
+
+    }
+
+   
+}
---
+++ Sun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    [SerializeField]
+    private float secondPerRealTimeSecound; // 게임 세계의 100초 = 현실 세계의 1초
+
+    private bool isNight = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right,0.1f * secondPerRealTimeSecound * Time.deltaTime);
+
+    }
+}
